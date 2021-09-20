d49f86c99545ae9fd1c4605f50fbd3cb584d2f00
clitile <xiangwhao@163.com>
2021-09-20 18:55:04 +0800

    add test

--- src/test/java/tut4/AppTest.java
+++ src/test/java/tut4/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import static tut4.Calc.add;
+import static tut4.Calc.substract;
 
 /**
  * Unit test for simple App.
@@ -16,5 +17,9 @@ public class AppTest
     public void testApp()
     {
         assertEquals(3, add(1, 2));
+    }
+    public void testApp2()
+    {
+        assertEquals(3, substract(1, 2));
     }
 }
